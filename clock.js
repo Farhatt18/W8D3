1e8cc69d30ae566ad29dc991ea13ca070f16af8a
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2023-12-06 15:07:27 -0500

    completed clock

---
+++ clock.js
@@ -0,0 +1,30 @@
+class Clock {
+  constructor(hour, minute,second){
+    const current_time = new Date(); 
+
+    this.hour = current_time.getHours();
+    this.minute = current_time.getMinutes();
+    this.second = current_time.getSeconds();
+    setInterval(this._tick.bind(this),1000);
+  }
+
+  printTime(){
+    console.log(`${this.hour}:${this.minute}:${this.second}`);
+  }
+
+  _tick(){
+    if (this.second < 60){
+      this.second++
+    } else if (this.second === 60){
+        this.second = 0;
+        this.minute++;
+    } else if (this.minute === 60){
+        this.minute = 0;
+        (this.hour ++) %24;
+    }
+  return this.printTime();
+  }
+
+};
+
+const clock = new Clock();
